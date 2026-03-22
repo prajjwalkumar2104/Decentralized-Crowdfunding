f53007211af071c07f7e8190210a97adeb110188
divyanshu978 <divyanshuuttam978@gmail.com>
2026-03-23 00:31:46 +0530

    Resolve conflicts for frontend start interacted with contract

---
+++ src/lib/wagmiConfig.js
@@ -0,0 +1,11 @@
+import { createConfig, http } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { injected } from "wagmi/connectors";
+
+export const config = createConfig({
+  chains: [sepolia], // change if mainnet
+  connectors: [injected()],
+  transports: {
+    [sepolia.id]: http(),
+  },
+});
